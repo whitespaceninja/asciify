bc14f81e43634c9aad09c2f7d77c734541da9a40
David Bachowski <dave@whitespace.ninja>
2017-01-28 01:14:56 -0500

    Splitting out background and context

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # asciify
 
-Adds an 'Asciify' selection for your right click image context menu. Clicking Asciify will print out an ascii version of that image to the background console. Future versions will print it out somewhere better :)
-
-To get to the background console, go to chrome://extensions and find the Asciify extension. Click the 'Inspect views: background.html' link and open up dev tools. Make the console window really big and shrink the hell out of the text by pressing 'Command -' a million times.
+Adds an 'Asciify' selection for your right click image context menu. Clicking Asciify will print out an ascii version of that image to the console. Future versions will attempt to replace the image within the browser.
--- background.js
+++ background.js
@@ -1,32 +1,43 @@
-"use strict";
+/*********************************************
+* Set up the context menu
+*********************************************/
 
 // Add 'Asciify' to the right-click menu for images
 chrome.contextMenus.create({
     title: "Asciify",
     contexts:["image"],  // ContextType
-    onclick: (function(image) {
-	/*var nodesArray = [].slice.call(document.getElementsByTagName('img'));
-	var inputs = nodesArray.filter(function(e) {
-	    e.src == image.srcUrl;
-	});
-
-	if (inputs.length > 0) {
-	    inputs[0].src = 'http://blogs-images.forbes.com/kristintablang/files/2016/02/UberPuppyBowl-1200x675.jpg';
-	}*/
-
-	var renderer = new Renderer();
-	renderImage(image, renderer);
+    onclick: (function(imageInput) {	
+	createCanvas();
+	var inverse = false;
+	var imgAsciified = new ImageAsciified(inverse);
+	imgAsciified.onload = function(characters) {
+	    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {text: 'render', charData: characters}, null);
+	    });
+	};
+	imgAsciified.load(imageInput.srcUrl);
     })
 });
+
+
+/*********************************************
+* Helper methods for canvas manipulation
+*********************************************/
 
 // if someone creates an id with this I want to meet them
 var UNIQUE_CANVAS_ID = 'OhOhHereSheComeWwatchOutBoyShellChewYouUp';
 
 var createCanvas = function() {
+    var canvas = document.getElementById(UNIQUE_CANVAS_ID)
+    if (canvas !== null) {
+	// already exists, move along
+	return;
+    }
+    
     // create a canvas that we can continually draw to
-    var canv = document.createElement('canvas');
-    canv.id = UNIQUE_CANVAS_ID;
-    document.body.appendChild(canv);    
+    canvas = document.createElement('canvas');
+    canvas.id = UNIQUE_CANVAS_ID;
+    document.body.appendChild(canvas);    
 };
 
 var getCanvas = function() {
@@ -55,115 +66,11 @@ var drawImageToCanvas = function(jsImage, canvas) {
     return true;
 }
 
-var renderImage = function(imageInput, renderer) {
-    createCanvas();
-    var inverse = false;
-    var imgAsciified = new ImageAsciified(inverse);
-    imgAsciified.onload = function(characters) {
-	renderer.addCharacterList(characters);
-	renderer.render();
-    };
-    imgAsciified.load(imageInput.srcUrl);
-};
 
-class Renderer {
-    constructor() {
-	// list of all characters to draw in no particular order.
-	// It is possible to have more than one character at the
-	// same location. It will draw the first one in the list.
-    	this.characters = [];
+/*********************************************
+* Wrapper around an ascii character
+*********************************************/
 
-	// the maximum X location of any character in our list
-	this.maxX = 0;
-	// same with Y location
-	this.maxY = 0;
-    }
-
-    clearScreen() {
-	// escape sequence required to clear the screen and set cursor at 0,0
-	console.log("\u001b[2J\u001b[0;0H");
-    }
-
-    render() {
-	for (var row = 0; row < this.maxY; row++) {
-	    var output = '';
-
-	    // find all characters that need to be drawn in this row
-	    var charactersInRow = this.getCharactersInRow(row);
-	    
-	    // if there aren't any, draw a blank line
-	    if (charactersInRow.length <= 0) {
-	   	for (var col = 0; col < this.maxX; col++) {
-		    output = output + ' ';
-		}
-	    } else {
-		// else there must be characters here, sort them first...
-		charactersInRow.sort(this.compareX);
-		
-		// ...then draw them all. Put it all in one string for quick render.
-		output = output + this.getOutputLine(charactersInRow);
-	    }
-
-	    console.log(output);
-	}
-    }
-
-    addCharacter(character) {
-	this.characters.push(character);
-	this.maxX = Math.max(this.maxX, character.x);
-	this.maxY = Math.max(this.maxY, character.y);
-    }
-
-    addCharacterList(characterList) {
-	var that = this;
-	characterList.map(function(c) { that.addCharacter(c); });
-    }
-
-    removeAllCharacters(character) {
-	this.characters = [];
-    }
-
-    compareX(a,b) {
-	if (a.x < b.x)
-	    return -1;
-	if (a.x > b.x)
-	    return 1;
-	return 0;
-    }
-
-    findCharacterAtX(characterList, x) {
-	var index = characterList.map(function(c) { return c.x; }).indexOf(x);
-	if (index < 0) {
-	    return null;
-	}
-
-	return characterList[index];
-    }
-
-    getCharactersInRow(row) {
-	return this.characters.filter(function(c) { return c.y == row; });
-    }
-
-    getOutputLine(charactersInRow) {
-	// ...then draw them all. Put it all in one string for quick render.
-	var output = '';        
-	for (var x = 0; x < this.maxX; x++) {
-	    var charAtX = this.findCharacterAtX(charactersInRow, x);
-
-	    if (charAtX != null) {
-		output = output + charAtX.symbol;
-	    } else {
-		output = output + ' ';
-	    }
-	}
-
-	return output;
-    }
-}
-
-/**
-* Model for an ascii character
-*/
 class Character {
     constructor(initialX, initialY, symbol) {
 	// where on the map are we drawing it
@@ -174,6 +81,11 @@ class Character {
     }
 }
 
+
+/*********************************************
+* Ascii-based manipulations to translate
+* images to a list of Character objects
+*********************************************/
 
 class ImageAsciified {
     constructor(inverse) {
